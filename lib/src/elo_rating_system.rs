c81d7245b42e5ff8a2a55910732d00f80e4a6cb1
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-10-13 20:37:11 +0200

    Added externresult in case of properties

--- lib/src/elo_rating_system.rs
+++ lib/src/elo_rating_system.rs
@@ -4,18 +4,18 @@ use hdk::prelude::*;
 pub trait EloRatingSystem {
     type GameInfo: TryFrom<SerializedBytes> + TryInto<SerializedBytes>;
     // Initial rating for a player who hasn't played any games
-    fn initial_rating() -> EloRating {
-        1000
+    fn initial_rating() -> ExternResult<EloRating> {
+        Ok(1000)
     }
 
     // This is the maximum gain or loss of ELO that a match can affect
-    fn k_factor() -> u32 {
-        32
+    fn k_factor() -> ExternResult<u32> {
+        Ok(32)
     }
 
     // How long we are going to wait until retrying to publish the already finished game results
-    fn unpublished_games_retry_interval_in_seconds() -> u32 {
-        60 * 5
+    fn unpublished_games_retry_interval_in_seconds() -> ExternResult<u32> {
+        Ok(60 * 5)
     }
 
     fn validate_game_result(
